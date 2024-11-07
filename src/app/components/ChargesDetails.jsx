84c59a43bb43a5d19d92bb1326cd36bdbc748ffe
AngeloKwakye <dangelokwakye@gmail.com>
2024-11-07 14:45:27 +0000

    1. telcos data restructured 2. change details componenet code format

--- src/app/components/ChargesDetails.jsx
+++ src/app/components/ChargesDetails.jsx
@@ -2,23 +2,23 @@ import { Card } from "@nextui-org/react";
 
 export const ChargesDetails = ({ totalDetails }) => {
   return (
-      <div className="mt-4 text-sm flex flex-col gap-2">
-        <p>
-          <span className="font-bold">Amount: </span>
-          {totalDetails.amount} GH&#8373;
-        </p>
-        <p>
-          <span className="font-bold">Charges:</span> {totalDetails.charges.toFixed(2)}{" "}
-          GH&#8373;
-        </p>
-        <p>
-          <span className="font-bold">E-LEVY:</span>{" "}
-          {totalDetails.e_levy.toFixed(2)} GH&#8373;
-        </p>
-        <p>
-          <span className="font-bold">Total Charge:</span>{" "}
-          {totalDetails.total_charges.toFixed(2)}{" "}GH&#8373;
-        </p>
-      </div>
+    <div className="mt-4 text-sm flex flex-col gap-2">
+      <p>
+        <span className="font-bold">Amount: </span>
+        {totalDetails.amount} GH&#8373;
+      </p>
+      <p>
+        <span className="font-bold">Charges:</span>{" "}
+        {totalDetails.charges.toFixed(2)} GH&#8373;
+      </p>
+      <p>
+        <span className="font-bold">E-LEVY:</span>{" "}
+        {totalDetails.e_levy.toFixed(2)} GH&#8373;
+      </p>
+      <p>
+        <span className="font-bold">Total Charge:</span>{" "}
+        {totalDetails.total_charges.toFixed(2)} GH&#8373;
+      </p>
+    </div>
   );
 };
